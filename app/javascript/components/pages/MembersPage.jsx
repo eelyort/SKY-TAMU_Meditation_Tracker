08281b8df3bc81470ab54fb44dc64425ae192d82
root <root@localhost>
2021-10-04 01:28:30 +0000

    members page dummy created

--- app/javascript/components/pages/MembersPage.jsx
+++ app/javascript/components/pages/MembersPage.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 const MembersPage = (props) => {
     return (
         <>
-            <h1>Members</h1>
+            <h1>Members   Admin View</h1>
+            <p> Troy Lee:  <a href = "mailto: troy@tamu.edu">Send Email</a> - Phone: 5255255156 -
+            <button type="button" onclick="alert('Hello world!')">Show</button> - <button type="button"
+             onclick="alert('Hello world!')">edit</button> - <button type="button" onclick="alert('Hello world!')">delete</button></p>
+            <p> Pranav Kumar:  <a href = "mailto: pk@tamu.edu">Send Email</a> - Phone: 6256256345 -
+            <button type="button" onclick="alert('Hello world!')">Show</button> - <button type="button"
+             onclick="alert('Hello world!')">edit</button> - <button type="button" onclick="alert('Hello world!')">delete</button></p>
+            <p> Shawn Martinez:  <a href = "mailto: shawn@tamu.edu">Send Email</a> - Phone: 5342625266 -
+            <button type="button" onclick="alert('Hello world!')">Show</button> - <button type="button"
+             onclick="alert('Hello world!')">edit</button> - <button type="button" onclick="alert('Hello world!')">delete</button></p>
+            <p> Austin Pitts:  <a href = "mailto: austin@tamu.edu">Send Email</a> - Phone: 9012342345 -
+            <button type="button" onclick="alert('Hello world!')">Show</button> - <button type="button"
+             onclick="alert('Hello world!')">edit</button> - <button type="button" onclick="alert('Hello world!')">delete</button></p>
+            <p> Dani Hott:  <a href = "mailto: dani@tamu.edu">Send Email</a> - Phone: 1867172734 -
+            <button type="button" onclick="alert('Hello world!')">Show</button> - <button type="button"
+             onclick="alert('Hello world!')">edit</button> - <button type="button" onclick="alert('Hello world!')">delete</button></p>
+            <p> Markus Link: <a href = "mailto: markus@tamu.edu">Send Email</a> - Phone: n/a -
+            <button type="button" onclick="alert('Hello world!')">Show</button> - <button type="button"
+             onclick="alert('Hello world!')">edit</button> - <button type="button" onclick="alert('Hello world!')">delete</button></p>
+
         </>
     );
 }
