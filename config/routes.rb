8f7afd053d14ebf075fd860826f0d941990f2c16
Troy Lee <troylee3006@gmail.com>
2021-10-22 01:11:06 -0500

    Update routes.rb
    Fix error on dev app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :events, param: :id
+    end
+  end
+  
+  namespace :api do
+    namespace :v1 do
       # example
       get 'recipes/index'
       post 'recipes/create'
