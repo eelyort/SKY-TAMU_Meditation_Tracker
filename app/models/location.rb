bf6476081863247c69e7ca685c222c28ba8c1de6
apitts175 <80348388+apitts175@users.noreply.github.com>
2021-10-25 20:22:07 -0500

    location unit tests (#26)
    * added Location scaffold and migration
    
    * added location input boxes to edit event page
    
    * finished location frontend
    
    * added showing locations to show events page
    
    * added location unit tests
    
    i dont know why but the gemfile changed after bundle install

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  validates :location_id, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }, uniqueness: true
+  validates :event_id, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
