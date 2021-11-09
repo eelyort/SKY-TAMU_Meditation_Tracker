ae78b4624e46c573cb8a98adcef5a6836b8600d9
Troy Lee <troylee3006@gmail.com>
2021-11-08 20:00:52 -0600

    basic oauth flow done

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -2,11 +2,39 @@ import React from "react";
 import MenuIcon from '@mui/icons-material/Menu';
 import { Menu, IconButton, Button, Typography, Toolbar, AppBar, MenuItem } from '@mui/material';
 import { Link } from "react-router-dom";
+import { GoogleAPI, GoogleLogin, GoogleLogout } from 'react-google-oauth';
 
 const menuItems = [{'text': 'Events', 'url': '/events'}, {'text': 'Members', 'url': '/members'}, {'text': 'Social Media', 'url': '/socialmedia'}, {'text': 'Attendance', 'url': '/attendance'}];
 
 const Header = () => {
     const [menuAnchor, setMenuAnchor] = React.useState(null);
+
+    const responseGoogle = (response) => {
+        console.log("\n----------------------------------------------\n" +
+            "responseGoogle()\n" +
+            "----------------------------------------------");
+        console.log(response);
+
+        var token = google_response.Zi;
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${google_response.Zi.accessToken}`,
+                'Content-Type': 'application/json',
+                'access_token': `${google_response.Zi.accessToken}`
+            },
+            body: JSON.stringify(token)
+        };
+
+        return fetch(`backend rails api url to google sign in path`, requestOptions)
+            .then(response => {
+                Cookie.set('accesstoken', response.headers.get('access-token'), { expires: 7 });
+                Cookie.set('client',response.headers.get('client'), { expires: 7 });
+                Cookie.set('tokentype',response.headers.get('token-type'), { expires: 7 });
+                Cookie.set('expiry',response.headers.get('expiry'), { expires: 7 });
+                Cookie.set('uid', response.headers.get('uid'),{ expires: 7 });
+            });
+    }
 
     return (
         <AppBar position="static" color="secondary">
@@ -45,6 +73,11 @@ const Header = () => {
                 </Button>
                 <div className="flex-spacer" />
                 <Button color="inherit" component={Link} to={"/login"}>Login</Button>
+                <GoogleAPI className="GoogleLogin" clientId="117887850590-ekck049bkcopmo0v73v5f7mt4b7afm5r.apps.googleusercontent.com">
+                    <div>
+                        <GoogleLogin height="10" width="500px" backgroundColor="#4285f4" access="offline" scope="email profile" onLoginSuccess={responseGoogle} onFailure={responseGoogle}/>
+                    </div>
+                </GoogleAPI>
             </Toolbar>
         </AppBar>
     );
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '777458461645-kr4fvhfu9eqgndbems4bcukj9eso4330.apps.googleusercontent.com', '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', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '117887850590-ekck049bkcopmo0v73v5f7mt4b7afm5r.apps.googleusercontent.com', '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', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
