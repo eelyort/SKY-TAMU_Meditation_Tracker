f5bf8bfad262c8d135fe730b4c05c9c3c21bdf3c
apitts175 <apitts175@gmail.com>
2021-11-13 19:37:59 -0600

    added location field to add event page

--- app/javascript/components/pages/events/AddEventForm.jsx
+++ app/javascript/components/pages/events/AddEventForm.jsx
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function AddEventForm(props) {
+
+    const [inputList, setInputList] = useState([{ virtual_link: "", building: "", room: "", city: "", stateloc: "", date: "", time: "" }]);
+
+    /*
+    const getInputList = () => {
+        const url = "/api/v1/locations";
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then(response => setInputList([...response, { virtual_link: "", building: "", room: "", city: "", stateloc: "", date: "", time: "" }]))
+    }
+    */
+
+    // handle input change
+    const handleInputChange = (e, index) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        const list = [...inputList];
+        list[index][name] = value;
+        setInputList(list);
+
+        if(index != inputList.length-1){
+            const { virtual_link, building, room, city, stateloc, date, time, id } = inputList[index];
+            const event_id = 0
+
+            const body = {
+                event_id,
+                virtual_link,
+                building,
+                room,
+                city,
+                stateloc,
+                date,
+                time
+            };
+
+            // props.databaseRequest("PATCH", body, id, "/api/v1/locations", () => getInputList());
+        }
+    };
+
+    // handle click event of the Remove button
+    const handleRemoveClick = (e, index) => {
+        e.preventDefault();
+        const answer = window.confirm("WARNING! \n Are you sure you would like to delete this location?");
+
+        if (answer) {
+            const location_id = inputList[index].id
+
+            // props.databaseRequest("DELETE", {}, location_id, "/api/v1/locations", () => getInputList())
+        }
+
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list);
+    };
+
+    // handle click event of the Add button
+    const handleAddClick = () => {
+        const answer = window.confirm("Are you sure you would like to add this location?");
+        if (answer) {
+            const { virtual_link, building, room, city, stateloc, date, time, id } = inputList[inputList.length-1];
+            const event_id = 0
+    
+            const body = {
+                event_id,
+                virtual_link,
+                building,
+                room,
+                city,
+                stateloc,
+                date,
+                time
+            };
+            // props.databaseRequest("POST", body, id, "/api/v1/locations", () => getInputList());
+        }
+
+        setInputList([...inputList, { virtual_link: "", building: "", room: "", city: "", stateloc: "", date: "", time: "" }]);
+    };
 
     return (props.trigger) ? (
         <>
@@ -34,6 +116,64 @@ function AddEventForm(props) {
                             placeholder="Event Description..." 
                             onChange={props.changeFunc} 
                             required/>
+                        
+                        <label>Locations</label>
+                        {inputList.map((x, i) => {
+                                return (
+                                   
+                                <div className="box">
+                                    <input
+                                    name="virtual_link"
+                                    placeholder="Enter Virtual Link"
+                                    value={x.virtual_link}
+                                    onChange={e => handleInputChange(e, i)}
+                                    />
+                                    <input
+                                    name="building"
+                                    placeholder="Enter Building"
+                                    value={x.building}
+                                    onChange={e => handleInputChange(e, i)}
+                                    />
+                                    <input
+                                    name="room"
+                                    placeholder="Enter Room"
+                                    value={x.room}
+                                    onChange={e => handleInputChange(e, i)}
+                                    />
+                                    <input
+                                    name="city"
+                                    placeholder="Enter City"
+                                    value={x.city}
+                                    onChange={e => handleInputChange(e, i)}
+                                    />
+                                    <input
+                                    name="stateloc"
+                                    placeholder="Enter State"
+                                    value={x.stateloc}
+                                    onChange={e => handleInputChange(e, i)}
+                                    />
+                                    <input
+                                    name="date"
+                                    value={x.date}
+                                    onChange={e => handleInputChange(e, i)}
+                                    type="date"
+                                    />
+                                    <input
+                                    name="time"
+                                    value={x.time}
+                                    onChange={e => handleInputChange(e, i)}
+                                    type="time"
+                                    />
+                                    <div className="btn-box">
+                                    {inputList.length !== 1 && <button
+                                        className="mr10"
+                                        onClick={e => handleRemoveClick(e, i)}>Remove</button>}
+                                    {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+                                    </div>
+                                </div>
+                                );
+                            }
+                        )}
 
                         <input 
                             className='submitStyle'
