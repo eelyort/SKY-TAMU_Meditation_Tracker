1d137ac155f0b8b83cbbc67aff3ad40463b190c1
Pranav Kumar <90021264+pranavPk1@users.noreply.github.com>
2021-11-30 00:10:57 -0600

    homepage

--- app/javascript/components/pages/HomePage.jsx
+++ app/javascript/components/pages/HomePage.jsx
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import HeroImage from 'images/iStock-1161561165.jpg';
 import Carousel from 'react-material-ui-carousel'
 import { Button, Typography, Paper } from '@mui/material';
 import { Link } from "react-router-dom";
+import { Card, CardContent,  CardHeader, CardActionArea } from '@mui/material';
+import AddEventForm from './events/AddEventForm'
+import EditEvent from './events/EditEvent'
+import AttendancePage from './AttendancePage'
+//import DateTime from DateTime;
+//import DateTime from 'react-datetime';
 
+
+var tot_time = 0
+var count1 = 0
 function Item(props)
 {
     return (
-        <Paper elevation={6}>            
+        <Paper elevation={6}>
             <div className="wrapper homepage">
                 <div className="homepage-hero">
                     <div className='overlay'>
                         <img src={props.item.image} />
                     </div>
-                    
+
                     <div className='shading overlay'></div>
-                    
+
                     <div className='text'>
                         <Typography variant="h1">
                             {props.item.name}
                         </Typography>
-                        
+
                         <Typography variant="h4">
                             {props.item.description}
                         </Typography>
 
                         <Button
-                            variant="contained" 
+                            variant="contained"
                             color="secondary"
-                            component={Link} 
-                            to={String('/'+ props.item.link)} 
+                            component={Link}
+                            to={String('/'+ props.item.link)}
                             onClick={ () => {console.log(String('/'+ props.item.link), "button clicked.")} }
                         >
                             {props.item.button}
@@ -42,11 +51,185 @@ function Item(props)
 }
 
 const HomePage = (props) => {
+   var retData;
+  const [inputList, setInputList] = useState([{ virtual_link: "", building: "", room: "", city: "", stateloc: "", start_time: "", end_time: "" }]);
+  const getInputList = () => {
+      const url = "/api/v1/locations";
+      fetch(url)
+          .then(response => {
+              if (response.ok) {
+                 //retData = response.json();
+                  return response.json();
+              }
+              throw new Error("Network response was not ok.");
+          })
+          .then(response => {
+            console.log(response);
+            //setInputList(retData);
+            setInputList([...response, { virtual_link: "", building: "", room: "", city: "", stateloc: "", start_time: "", end_time: "" }])
+          })
+         // .then(response => setInputList( response ))
+         .catch(error => console.log(error));
+  }
+
+/*  function loadInputList() {
+    useEffect(() => {
+      getInputList()
+    }, []);
+  }*/
+
+
+// const [attendances, setAttendances] = React.useState(undefined);
+const [attendances, setAttendances] = useState([]);
+console.log("Here")
+
+// componentDidMount
+ const fetchAttendance = () => {
+    const url = "/attendances";
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Network response was not ok.");
+        })
+        .then(response => {
+            console.log(response);
+            setAttendances(response);
+
+        })
+        .catch(err => console.log("Error: " + err));
+}
+
+/* const fetchAttendance = () => {
+    const url = "/attendances";
+    const response = fetch(url);
+    const json = response.json;
+    setAttendances(json.data);
+    console.log(attendances);
+  }
+  */
+
+React.useEffect(() => {
+    fetchAttendance();
+    getInputList();
+    console.log(attendances);
+}, []);
+
+
+
+const getTimeTracker = () => {
+
+   var count;
+   var loop = 0;
+   var time_difference;
+   //getInputList();
+//   console.log("before inputlist log")
+
+  // console.log(inputList)
+  console.log("Length of Locations " + inputList.length);
+    inputList.forEach(location => {
+      count = 0;
+      loop++;
+//      console.log("Inside inputList");
+//    console.log(location.city);
+     attendances.forEach(attendance => {
+//       console.log("Inside attendance");
+//       console.log(attendances.RSVP);
+       if((attendances.RSVP = 'Yes') && (attendance.location_id == location.id && attendance.event_id == location.event_id))
+           count++;
+//           console.log(count);
+      });
+      console.log("Loop " + loop);
+
+      if(location.start_time != ''){
+        var start = new Date(location.start_time);
+  //      var startTime = Date.parse(start);
+        var startHours = start.getHours();
+        var startMin = start.getMinutes();
+        var startDay = start.getDate();
+        var startMonth = start.getMonth();
+        var startYear = start.getYear();
+  //      console.log("start time " + startHours + " : " + startMin);
+        var end = new Date(location.end_time);
+        var endHours = end.getHours();
+        var endMin = end.getMinutes();
+        var endDay = end.getDate();
+        var endMonth = end.getMonth();
+        var endYear = end.getYear();
+
+        var startTotalDays = startYear * startMonth + startDay;
+        var endTotalDays = endYear * endMonth + endDay;
+        console.log("start days " + startTotalDays + "end days " + endTotalDays);
+//        var startTotalHours = startTotalDays * startHours;
+//        var endTotalHours = endTotalDays * endHours;
+//        console.log("start hours " + startTotalHours);
+  //      console.log("end hours " + endTotalHours);
+       if(endTotalDays!=startTotalDays){
+          if(startHours > endHours){
+            time_difference = (Math.abs(endTotalDays - startTotalDays) * 24)
+             - Math.abs(endHours - startHours);
+          }
+          else if (startHours == endHours) {
+            time_difference = Math.abs(endTotalDays - startTotalDays) * 24;
+          }
+          else{
+            time_difference = Math.abs(endHours - startHours) +
+             (Math.abs(endTotalDays - startTotalDays) * 24);
+          }
+        }
+        else{
+          if(startHours == endHours){
+            time_difference = 1;
+          }
+          else{
+            time_difference = Math.abs(endHours - startHours);
+          }
+        }
+
+  //      console.log(start);
+  //      console.log(end);
+        console.log("start time " + location.start_time);
+        console.log("end time " + location.end_time);
+        console.log("time difference " + time_difference);
+  //      var start_min = start.getMinutes();
+        count1 += count;
+        tot_time += count * time_difference;
+      }
+/*      var start = new Date(location.start_time);
+      var startTime = Date.parse(start);
+      var startHours = start.getHours();
+      var startMin = start.getMinutes();
+      console.log("start time " + startHours + " : " + startMin);
+      var end = new Date(location.end_time);
+      var endHours = end.getHours();
+      var endMin = end.getMinutes();
+
+      var time_difference = Math.abs(endHours-startHours);
+
+//      console.log(start);
+//      console.log(end);
+
+//      var start_min = start.getMinutes();
+      count1 += count;
+      tot_time += count * time_difference;*/
+   });
+//   return tot_time;
+
+
+}
+//React.useEffect(() => {
+    getTimeTracker();
+//    console.log(tot_time);
+//}, []);
+
+
+
 
     var CarouselItems = [
         {
-            name: "1000 Hours Meditated Total",
-            description: "# of people expected at next session",
+            name: tot_time + " Hours Meditated Total",
+            description: count1 + " of people expected at next session",
             button: "See Events",
             link: "events",
             image: HeroImage
@@ -77,13 +260,13 @@ const HomePage = (props) => {
                     animation = {"slide"}
                     navButtonsAlwaysVisible = {true}
                     next={ (next, active) => { console.log(`we left ${active}, and are now at ${next}`); } }
-                    prev={ (prev, active) => { console.log(`we left ${active}, and are now at ${prev}`); } } 
+                    prev={ (prev, active) => { console.log(`we left ${active}, and are now at ${prev}`); } }
 
                 >
                     { CarouselItems.map( (item, i) => <Item key={i} item={item} /> ) }
                 </Carousel>
             </div>
-            
+
             {/* <div className="wrapper homepage">
                 <div className="homepage-hero">
                     <div className={'overlay'}>
