7385a1f1533f19c32b92d024b9631eb2401fc725
shawn-martinez821 <shawn_martinez821@tamu.edu>
2021-12-05 17:24:18 -0600

    Mobile Friendly "Members" and "About Us"
    Changed the padding for members page. Changed the image size on about us page.

--- app/javascript/components/pages/AboutPage.jsx
+++ app/javascript/components/pages/AboutPage.jsx
@@ -10,21 +10,13 @@ const AboutPage = (props) => {
         fontFamily: "Arial, Helvetica, sans-serif"
     }
 
-    const AboutBody = {
-        margin: "auto",
-        width: "80%",
-        padding: "10px",
-        fontSize: "1.25em",
-        fontFamily: "Arial, Helvetica, sans-serif"
-    }
-
     return (
         <>
             <div style={AboutHeader}>
                 <h1>About Page</h1>
             </div>
 
-            <div style={AboutBody}>
+            <div className="about-wrapper">
 
                 <h4>About Us</h4>
                 <p>SKY@TAMU is a Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreet dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
